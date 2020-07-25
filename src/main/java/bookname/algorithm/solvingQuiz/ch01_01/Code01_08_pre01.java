5364327a0391ac826e0af9ef2d0e0c9f895fc6d8
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-07-25 22:32:00 +0900

    [add] bookname.algorithm.solvingQuiz에서 ch01_01의 01-08 예비문제1 추가
    - 추가
      - bookname/algorithm/solvingQuiz/ch01_01/Code01_08_pre01
      - 내용 : 0과 1로 이루어진 n길이의 문자열에 0은 연속하여 나올 수 없을때, 길이가n인 문자열은 몇개일까

---
+++ src/main/java/bookname/algorithm/solvingQuiz/ch01_01/Code01_08_pre01.java
@@ -0,0 +1,49 @@
+package bookname.algorithm.solvingQuiz.ch01_01;
+
+public class Code01_08_pre01 {
+  static long startDate;
+
+  public static void main(String[] args) {
+    final long lengthOfString = 40;
+
+    System.out.println("<재귀함수를 사용하는 경우>");
+    startDate = System.currentTimeMillis();
+    System.out.println(countBinaryNum1(lengthOfString) + "가지입니다."
+        + "\n-> 소요시간은 " + (System.currentTimeMillis() - startDate) + "밀리초 입니다.");
+
+    System.out.println("\n<for반복문을 사용하는 경우>");
+    startDate = System.currentTimeMillis();
+    System.out.println(countBinaryNum2(lengthOfString) + "가지입니다."
+        + "\n-> 소요시간은 " + (System.currentTimeMillis() - startDate) + "밀리초 입니다.");
+  }
+
+  // using recursive function
+  private static long countBinaryNum1(long param) {
+    if(param == 1)
+      return 2;
+    if(param == 2)
+      return 3;
+    return countBinaryNum1(param-1) + countBinaryNum1(param-2);
+  }
+
+  // using for loop
+  private static long countBinaryNum2(long param) {
+    if(param == 1)
+      return 2;
+    if(param == 2)
+      return 3;
+
+    long preprev = 2;
+    long prev = 3;
+    long result = 0;
+    for(int i = 3; i <= param; i++) {
+      result = prev + preprev;
+      preprev = prev;
+      prev = result;
+    }
+    return result;
+  }
+
+
+}
+
