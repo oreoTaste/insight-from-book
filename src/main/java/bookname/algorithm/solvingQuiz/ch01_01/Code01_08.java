41c48ce4897bbc9436978077bd16520e0515afd6
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-07-27 23:48:00 +0900

    [add] bookname.algorithm.solvingQuiz에서 ch01_01의 01-08 추가
    - 추가
      - bookname/algorithm/solvingQuiz/ch01_01/Code01_08
      - 내용 : 예비문제2의결과에 중복계산을 제거하기

---
+++ src/main/java/bookname/algorithm/solvingQuiz/ch01_01/Code01_08.java
@@ -0,0 +1,68 @@
+package bookname.algorithm.solvingQuiz.ch01_01;
+
+public class Code01_08 {
+  static long[] memory;
+  static long startDate;
+
+  public static void main(String[] args) {
+    final long numOfStairs = 40;
+    memory = new long[(int) numOfStairs + 1];
+
+    System.out.println("<재귀함수를 사용하는 경우>");
+    startDate = System.currentTimeMillis();
+    System.out.println(countBinaryNum1(numOfStairs) + "가지입니다."
+        + "\n-> 소요시간은 " + (System.currentTimeMillis() - startDate) + "밀리초 입니다.");
+
+    System.out.println("\n<재귀함수를 사용하는 경우 (중복계산 없애기)>");
+    startDate = System.currentTimeMillis();
+    System.out.println(countBinaryNum2(numOfStairs) + "가지입니다."
+        + "\n-> 소요시간은 " + (System.currentTimeMillis() - startDate) + "밀리초 입니다.");
+
+    System.out.println("\n<for반복문을 사용하는 경우>");
+    startDate = System.currentTimeMillis();
+    System.out.println(countBinaryNum3(numOfStairs) + "가지입니다."
+        + "\n-> 소요시간은 " + (System.currentTimeMillis() - startDate) + "밀리초 입니다.");
+  }
+
+  // using recursive function
+  private static long countBinaryNum1(long param) {
+    if(param == 1)
+      return 1;
+    if(param == 2)
+      return 2;
+    return countBinaryNum1(param-1) + countBinaryNum1(param-2);
+  }
+
+  // using recursive function
+  private static long countBinaryNum2(long param) {
+    if(param == 1)
+      return memory[1] = 1;
+    if(param == 2)
+      return memory[2] = 2;
+    if(memory[(int) param] != 0)
+      return memory[(int) param];
+
+    return memory[(int) param] = countBinaryNum1(param-1) + countBinaryNum1(param-2);
+  }
+
+  // using for loop
+  private static long countBinaryNum3(long param) {
+    if(param == 1)
+      return 1;
+    if(param == 2)
+      return 2;
+
+    long preprev = 1;
+    long prev = 2;
+    long result = 0;
+    for(int i = 3; i <= param; i++) {
+      result = prev + preprev;
+      preprev = prev;
+      prev = result;
+    }
+    return result;
+  }
+
+
+}
+
