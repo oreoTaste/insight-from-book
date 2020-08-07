1c1431451dc044d5b9a84ac2e718968e234fda81
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-08-07 22:35:06 +0900

    [mod] bookname.algorithm.solvingQuiz에서 ch01_07의 01-15 수정
    - 추가
      - bookname/algorithm/solvingQuiz/ch01_07/Code01_15
      - 내용 : 그레이코드 출력하기

--- src/main/java/bookname/algorithm/solvingQuiz/ch01_07/Code01_15.java
+++ src/main/java/bookname/algorithm/solvingQuiz/ch01_07/Code01_15.java
@@ -2,36 +2,52 @@ package bookname.algorithm.solvingQuiz.ch01_07;
 
 import java.util.Scanner;
 
-// 그레이 코드 출력하기 (작성중)
+// 그레이 코드 출력하기
 public class Code01_15 {
   static Scanner scanner = new Scanner(System.in);
+  static int printIndex;
 
   public static void main(String[] args) {
     int[] code = new int[20];
     System.out.print("몇번째 숫자를 알고 싶습니까? ");
-    int input = Integer.parseInt(scanner.nextLine());
+    printIndex = Integer.parseInt(scanner.nextLine());
 
-    printGray(code, input, 0);
+    int input;
+    for(int i = 0; ; i++) {
+      if(printIndex < Math.pow(2, i)) {
+        input = i;
+        break;
+      }
+    }
+    printGray(code, input, 0, 0);
   }
 
-  private static void printGray(int[] code, int input, int index) {
-    if(index == input) {
-      printCode(code, input);
+  private static void printGray(int code[], int n, int index, int reverse) {
+
+    if(printIndex == 1) {
+      printCode(code, n);
+      printIndex = 0;
       return;
     }
 
-    code[index] = 0;
-    printGray(code, input, index+1);
-    code[index] = 1;
-    printGrayReverse(code, input, index+1);
-  }
+    if(index == n) {
+      //      printCode(code, n);
+      printIndex--;
+      return;
+    }
 
-  private static void printGrayReverse(int[] code, int input, int i) {
-    // 작성중
+    code[index] = reverse;
+    printGray(code, n, index + 1, 0);
+    code[index] = 1 - reverse;
+    printGray(code, n, index + 1, 1);
   }
 
   private static void printCode(int[] code, int input) {
-    // 작성중
+    System.out.print("-> ");
+    for(int i = 0; i < input; i++) {
+      System.out.print(code[i]);
+    }
+    System.out.println();
   }
 
 }
