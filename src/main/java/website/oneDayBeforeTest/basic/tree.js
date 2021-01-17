67331cc2f18c422b4f2c51b7306f217ca3f867d9
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2021-01-18 00:49:25 +0900

    tree practice

--- src/main/java/website/oneDayBeforeTest/basic/tree.js
+++ src/main/java/website/oneDayBeforeTest/basic/tree.js
@@ -83,6 +83,32 @@ function onelineBfsArrow(tree, start) {
   }
   return visited;
 }
+function multilineBfs(tree, start) {
+  const answer = [];
+  const visited = [];
+  const row = [start];
+  const Bfs = (tree, row, visited) => {
+    if (row.length > 0) {
+      visited.push(...row);
+      answer.push(row);
+    }
+
+    const next = [];
+    row.forEach((el) => {
+      if (tree[el]) {
+        next.push(...tree[el]);
+      }
+    });
+    if (next.length > 0)
+      Bfs(
+        tree,
+        next.filter((el) => !visited.includes(el)),
+        visited
+      );
+  };
+  Bfs(tree, row, visited);
+  return answer;
+}
 const tree = {
   100: new Set([67, 66]),
   67: new Set([100, 82, 63]),
@@ -94,7 +120,7 @@ const tree = {
   61: new Set([82]),
   79: new Set([82, 87, 77]),
   65: new Set([69, 84, 99]),
-  81: new Set([96]),
+  81: new Set([69]),
   87: new Set([79, 31, 78]),
   77: new Set([79]),
   84: new Set([65]),
@@ -102,9 +128,18 @@ const tree = {
   31: new Set([87]),
   78: new Set([87]),
 };
+function test(testStr, func, tree, start) {
+  let now = +new Date();
+  for (var i = 0; i < 100000; i++) {
+    func(tree, start);
+  }
+  console.warn(`${-now + (now = +new Date())}.ms 소요 : ${testStr}`);
+  console.log(">>", func(tree, start));
+}
 console.log("------------------------------------DFS------------------------------------");
-console.log("oneline", onelineDfs(tree, 100));
-console.log("multiline", multilineDfs(tree, 100));
+test("oneline Dfs", onelineDfs, tree, 100);
+test("multiline Dfs", multilineDfs, tree, 100);
 console.log("------------------------------------BFS------------------------------------");
-console.log("oneline", onelineBfs(tree, 100));
-console.log("oneline Arrow", onelineBfsArrow(tree, 100));
+test("oneline Bfs", onelineBfs, tree, 100);
+test("oneline Bfs Arrow", onelineBfsArrow, tree, 100);
+test("multiline Bfs", multilineBfs, tree, 100);
