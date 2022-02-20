bfbaa8dfc9708b8c06e0b2eb10dd2e49b539df83
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-02-20 23:33:27 +0900

    [mod] 이것이취업을위한코딩테스트다 quiz9-2 연습

--- src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-2.js
+++ src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-2.js
@@ -1,24 +1,26 @@
 // 개선된 다익스트라 (Priority Queue or Heap)
+// Stack (push, pop), Queue (enqueue, dequeue), Priority Queue(offer, poll)
 class Stack {
     #array = []
     #start = 0
     #end = 0
-    get size() {
-        return this.#end - this.#start
-    }
     get isEmpty() {
-        return this.#start == this.#end
+        if(this.#start == this.#end) {
+            this.#start = this.#end = 0
+            this.#array = []
+            return true
+        }
+        return false
     }
     push(item) {
-        this.#array[this.#end] = item
-        this.#end++
+        this.#array[this.#end++] = item
         return item
     }
     pop() {
-        if(!this.isEmpty) {
-            return this.#array[--this.#end]
+        if(this.isEmpty) {
+            return false
         }
-        return false
+        return this.#array[--this.#end]
     }
 }
 
@@ -26,21 +28,23 @@ class Queue {
     #array = []
     #start = 0
     #end = 0
-    get isEmpty(){
-        return this.#start == this.#end
+    get isEmpty() {
+        if(this.#start == this.#end) {
+            this.#start = this.#end = 0
+            this.#array = []
+            return true
+        }
+        return false
     }
     enqueue(item) {
         this.#array[this.#end++] = item
         return item
     }
-    dequeue() {
+    dequeue(){
         if(this.isEmpty) {
-            this.#start = this.#end = 0
             return false
         }
-        let item = this.#array[this.#start]
-        this.#start++
-        return item
+        return this.#array[this.#start++]
     }
 }
 
@@ -48,89 +52,50 @@ class PriorityQueue {
     #array = []
     #start = 0
     #end = 0
-    get isEmpty(){
-        return this.#start == this.#end
+    get isEmpty() {
+        if(this.#start == this.#end) {
+            this.#start = this.#end = 0
+            this.#array = []
+            return true
+        }
+        return false
     }
     binarySearch(start, end, priority) {
-        if(start >= end || start == end) {
+        let mid = parseInt((start + end)/2)
+        if(start == end || start > end) {
             return end
         }
-        let mid = parseInt((start + end)/2)
-
         if(this.#array[mid].priority == priority) {
             return mid
         } else if(this.#array[mid].priority > priority) {
-            return this.binarySearch(start, mid, priority)
-        } else {
             return this.binarySearch(mid+1, end, priority)
+        } else {
+            return this.binarySearch(start, mid, priority)
         }
     }
-    peek() {
-        return this.#array[this.#start]
-    }
-    offer(item, priority) {
-        let node = {item, priority}
-        if(this.isEmpty) {
-            this.#array[this.#end] = node
-            this.#end++
-            return node
-        }
-        let point = this.binarySearch(this.#start, this.#end, priority)
-        this.#array.splice(point,0,node)
+    offer(item, priority=item) {
+        let ind = this.binarySearch(this.#start, this.#end, priority)
+        this.#array.splice(ind, 0, {item, priority})
         this.#end++
-    }
-
-    poll() {
-        if(this.isEmpty) {
-            this.#start = this.#end = 0
-            return false
-        }
-        let item = this.#array[this.#start]
-        this.#start++
         return item
     }
-    get toString() {
-        return this.#array
+    poll() {
+        if(this.isEmpty) {
+            return false
+        }
+        return this.#array[this.#start++]
     }
 }
-
-// 개선된 다익스트라
-const [node, path] = '6 11'.split(' ').map(el => parseInt(el))
-const start = 1
-const GRAPH = '1 2 2 1 3 5 1 4 1 2 3 3 2 4 2 3 2 3 3 6 5 4 3 3 4 5 1 5 3 1 5 6 2'.split(' ').map(el => parseInt(el))
-
-let graph = new Map()
-for(let i = 0; i < path; i++) {
-    let [begin, end, dist] = GRAPH.slice(3 * i, 3 * (i+1))
-    graph.set(begin, [[end, dist], ...graph.get(begin) || []])
-}
-let visited = Array.from(Array(node + 1), (_)=>Number.MAX_SAFE_INTEGER)
-let confirm = Array.from(Array(node + 1), (_)=>false)
-visited[0]=0
-confirm[0]=true
 
 let pq = new PriorityQueue()
-function dijkstra() {
-    visited[start] = 0
-    confirm[start] = true
-    for([end, dist] of graph.get(start) || []) {
-        visited[end] = dist
-        pq.offer(end, dist)
-    }
-
-    while(!pq.isEmpty) {
-        let {item, priority} = pq.poll()
-        if(confirm[item] == true) {
-            break;
-        }
-        visited[item] = priority
-        confirm[item] = true
-        for(let [end, dist] of graph.get(item)||[]) {
-            pq.offer(end, visited[item] + dist)
-        }        
-    }
-}
-dijkstra()
-console.log(visited.slice(1,))
-console.log(confirm.slice(1,))
-
+pq.offer(2)
+pq.offer(5)
+pq.offer(3)
+pq.offer(1)
+pq.offer(4)
+console.log(pq.poll())
+console.log(pq.poll())
+console.log(pq.poll())
+console.log(pq.poll())
+console.log(pq.poll())
+console.log(pq.poll())
