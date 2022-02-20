f1c4c3977085a77799806739d92df5fab783078e
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-02-21 00:36:25 +0900

    [mod] 이것이취업을위한코딩테스트다 quiz9-2 연습2

--- src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-2.js
+++ src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-2.js
@@ -68,9 +68,9 @@ class PriorityQueue {
         if(this.#array[mid].priority == priority) {
             return mid
         } else if(this.#array[mid].priority > priority) {
-            return this.binarySearch(mid+1, end, priority)
-        } else {
             return this.binarySearch(start, mid, priority)
+        } else {
+            return this.binarySearch(mid+1, end, priority)
         }
     }
     offer(item, priority=item) {
@@ -85,17 +85,52 @@ class PriorityQueue {
         }
         return this.#array[this.#start++]
     }
+    get toString() {
+        return this.#array
+    }
 }
 
+const [node, path] = '6 11'.split(' ').map(el => parseInt(el))
+const start = 1
+const GRAPH = '1 2 2 1 3 5 1 4 1 2 3 3 2 4 2 3 2 3 3 6 5 4 3 3 4 5 1 5 3 1 5 6 2'.split(' ').map(el => parseInt(el))
+
+let graph = new Map()
 let pq = new PriorityQueue()
-pq.offer(2)
-pq.offer(5)
-pq.offer(3)
-pq.offer(1)
-pq.offer(4)
-console.log(pq.poll())
-console.log(pq.poll())
-console.log(pq.poll())
-console.log(pq.poll())
-console.log(pq.poll())
-console.log(pq.poll())
+for(let i = 0; i < path; i++) {
+    let [begin, end, dist] = GRAPH.slice(i * 3, (i+1) * 3)
+    graph.set(begin, [[end, dist], ...graph.get(begin) || []])
+}
+
+// 개선된 다익스트라
+let visited = Array(node+1).fill(Number.MAX_SAFE_INTEGER)
+let confirm = Array(node+1).fill(false)
+visited[0] = 0
+confirm[0] = true
+
+function dijkstra() {
+    visited[start] = 0
+    confirm[start] = true
+
+    for(let [end, dist] of graph.get(start) || []) { // start부터 시작하는 노선 찾기
+        visited[end] = dist
+        pq.offer([start, end], dist)
+    }
+
+    for(let i = 1; i < path; i++) {
+        let {item:[begin, end], priority} = pq.poll()
+
+        if(visited[begin] + priority < visited[end]) {
+            visited[end] = visited[begin] + priority
+        }
+
+        confirm[end] = true
+        for(let [end2, priority] of graph.get(end) || []) {
+            pq.offer([end, end2], priority)
+        }
+    }
+
+}
+
+dijkstra()
+console.log(visited.slice(1,))
+console.log(confirm.slice(1,))
