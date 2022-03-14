7cc6adeb68d201c67a7cafa2752043d21e3e6e75
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2022-03-15 00:00:10 +0900

    [mod] quiz9-4 연습

--- src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-4.js
+++ src/main/java/bookname/algorithm/이것이_취업을_위한_코딩_테스트다/9.최단경로/quiz9-4.js
@@ -7,26 +7,37 @@ let [x, k] = '4 5'.split(' ').map(el => parseInt(el))
 // let GRAPH = '1 3 2 4'.split(' ').map(el => parseInt(el))
 // let [x, k] = '3 4'.split(' ').map(el => parseInt(el))
 
-
-let graph = []
-for(let i = 0; i < line; i++) {
-    graph.push(GRAPH.slice(i * 2, (i+1) * 2))
-}
-
 let visited = Array.from(Array(node), (_) => Array(node).fill(Number.MAX_SAFE_INTEGER))
-for(let i = 0; i < node; i++){
-    visited[i][i] = 0
+console.log(visited)
+
+for(let i = 0; i < line; i++) {
+    let [start, end] = GRAPH.slice(i * 2, (i+1) * 2)
+    visited[start - 1][end - 1] = 1
+    visited[end - 1][start - 1] = 1
 }
-graph.forEach(([start,end]) => {visited[start-1][end-1] = 1; visited[end-1][start-1] = 1})
-function fwa(visited, node) {
+
+function fwa() {
+    // 1. 자기자신까지의 거리는 0이다.
+    for(let i = 0; i < node; i++) {
+        visited[i][i] = 0
+    }
+
+    // 2. 경유지를 하나 선택하고 거기를 거쳐가는 경우의 수 생각하기
     for(let i = 0; i < node; i++) {
         for(let j = 0; j < node; j++) {
-            for(let p = 0; p < node; p++) {
-                visited[j][p] = Math.min(visited[j][p], visited[j][i] + visited[i][p])
+            if(i == j) {
+                continue
+            }
+            for(let k = 0; k < node; k++) {
+                if(j == k) {
+                    continue
+                }
+                visited[j][k] = Math.min(visited[j][k], visited[j][i] + visited[i][k])
             }
         }
     }
 }
-fwa(visited,node)
-//console.log(visited)
-console.log(`${visited[0][k-1] + visited[k-1][x-1] > Number.MAX_SAFE_INTEGER ? -1 : visited[0][k-1] + visited[k-1][x-1]}건`)
+
+fwa()
+console.log(visited)
+console.log(`${visited[0][k-1] + visited[k-1][x-1] >= Number.MAX_SAFE_INTEGER ? -1 : visited[0][k-1] + visited[k-1][x-1]}`)
